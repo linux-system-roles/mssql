be10c4ed32d7626a0d7a1685fbd4d551e55475e2
Sergei Petrosian <spetrosi@redhat.com>
2021-03-25 10:24:34 +0100

    Add creating login and user to T-SQL, remove printing entries

--- tests/sql_script.sql
+++ tests/sql_script.sql
@@ -1,3 +1,5 @@
+CREATE LOGIN MyLogin WITH PASSWORD = 'p@55w0rD'
+CREATE USER MyUser FOR LOGIN MyLogin
 CREATE DATABASE ExampleDB
 GO
 USE ExampleDB
@@ -6,6 +8,4 @@ INSERT INTO Inventory VALUES (1, 'apple', 100)
 INSERT INTO Inventory VALUES (2, 'orange', 150)
 INSERT INTO Inventory VALUES (3, 'banana', 154)
 INSERT INTO Inventory VALUES (4, N'バナナ', 170)
-GO
-SELECT * FROM Inventory WHERE quantity > 100;
 GO
